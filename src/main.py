cd1914bc1549df3d7fe59fc14c9a3404c8f43e37
Dhaval Potdar <dhavalpotdar@gmail.com>
2023-09-19 06:08:35 +0000

    imported sys in main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-sys.path.append('/workspaces/descriptive-stats-using-ci/src')
+sys.path.append('/workspaces/descriptive-stats-using-ci/src') # noqa: F402
 from lib.functions import (
     load_crime_data,
     calculate_standard_deviation,
