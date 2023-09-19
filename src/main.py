0cbffeade7c3e456a6547c3f89ad0db6435276c6
Dhaval Potdar <dhavalpotdar@gmail.com>
2023-09-19 18:23:13 +0000

    passed lint

--- src/main.py
+++ src/main.py
@@ -72,7 +72,8 @@ if __name__ == "__main__":
 
     output_df = format_desc_stats(data_dict=data_dict)
 
-    with open("/workspaces/descriptive-stats-using-ci/outputs/DESC_STATS.md", "w+") as f:
+    with open("/workspaces/descriptive-stats-using-ci/outputs/DESC_STATS.md", 
+              "a+") as f:
         # Writing data to a file
         f.write("\n")
         f.writelines(output_df.to_markdown())
